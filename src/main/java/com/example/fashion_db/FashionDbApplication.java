a19b9529cb36abc2bce0e9567e4ced20f12aaf17
QuanThai <thaithanhquan11102005@gmail.com>
2026-04-19 16:08:23 +0700

    init project be

---
+++ src/main/java/com/example/fashion_db/FashionDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashion_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionDbApplication.class, args);
+    }
+
+}
